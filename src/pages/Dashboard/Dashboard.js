5a23a4432faf1ba89d4628fdd2825ad17d12130f
Sam Chang <sam21323@gmail.com>
2023-04-12 19:23:42 +0800

    Adding pinned notes

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,28 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/macro';
-import { DashboardContext } from '../../context/dashboardContext';
+import { db } from '../../firebase';
+import { onSnapshot, query, collection, where } from 'firebase/firestore';
+import Exit from '../../components/Buttons/Exit';
 
-const Wrapper = styled.div`
+const Notes = styled.div`
   width: 50%;
   margin: 50px auto;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  gap: 30px;
+`;
+
+const NoteContainer = styled.div`
+  position: relative;
 `;
 
 const PinnedNote = styled.div`
   border: 1px solid black;
   height: 200px;
   cursor: pointer;
+  position: relative;
 `;
 
 export default function Dashboard() {
-  const { pinnedNote } = useContext(DashboardContext);
+  const [pinnedNote, setPinnedNote] = useState(null);
+  useEffect(() => {
+    const unsub = onSnapshot(
+      query(
+        collection(db, 'Users', 'sam21323@gmail.com', 'Notes'),
+        where('pinned', '==', true)
+      ),
+      (querySnapshot) => {
+        const notes = [];
+        querySnapshot.forEach((doc) => {
+          notes.push({ content: doc.data(), id: doc.id, isVisible: true });
+        });
+        console.log(notes);
+        setPinnedNote(notes);
+        //   dataRef.current = notes;
+        //   setData(notes);
+      }
+    );
+    return unsub;
+  }, []);
+
+  if (!pinnedNote) return;
   return (
-    <Wrapper>
-      <PinnedNote
-        dangerouslySetInnerHTML={{ __html: pinnedNote }}
-        suppressContentEditableWarning
-      />
-    </Wrapper>
+    <Notes>
+      {pinnedNote.map((note) => (
+        <NoteContainer>
+          <PinnedNote
+            dangerouslySetInnerHTML={{ __html: note.content.context }}
+          />
+          <Exit top={0} right={0}>
+            X
+          </Exit>
+        </NoteContainer>
+      ))}
+    </Notes>
   );
 }
